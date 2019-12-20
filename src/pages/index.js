f27f16fdc30e77a5588e1d1e57ee10a95a640dc0
Daniele Tortora <tortora87@gmail.com>
2019-12-20 15:08:40 +0000

    setup axe for development mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Hero from "../components/hero/hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
+// if we're in dev mode
+if (process.env.NODE_ENV === "development") {
+  // Set up react-axe to catch accessibility issues during development
+  const axe = require("react-axe");
+  axe(React, ReactDOM, 1000);
+}
 
 AOS.init();
 
