93fcb4dc1bebbda0555007b366c1022b3993f49a
Daniele Tortora <tortora87@gmail.com>
2019-12-20 15:46:21 +0000

    create footer skeleton

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React from "react";
+import { css } from "@emotion/core";
+import { theme } from "../../styles";
+
+const { colors } = theme;
+
+const footer = css`
+  width: 100%;
+  height: 20rem;
+  background: ${colors.maastrichtBlue};
+  border-top: 2px solid ${colors.grey};
+`;
 
 const Footer = () => {
   return (
-    <div>
+    <footer css={footer}>
       <h1>Footer</h1>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
