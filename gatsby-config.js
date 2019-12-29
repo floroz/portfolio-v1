857723ddad91d1949ee4f24b58e7d237e9080277
Daniele Tortora <tortora87@gmail.com>
2019-12-29 13:06:53 +0100

    gatsby import plugin alias setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,17 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-alias-imports`,
+      options: {
+        aliases: {
+          "~components": "src/components",
+          "~containers": "src/containers",
+          "~styles": "src/styles",
+          "~images": "src/images",
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
