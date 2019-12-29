879b2e384fc2061ef609c816619950a68babaecc
Daniele <tortora87@gmail.com>
2019-12-29 21:07:15 +0100

    add aos into lifecycle to avoid server side error 'document' not found
    fix
    
    fix2
    
    fix3
    
    fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "aos/dist/aos.css";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 import Footer from "components/footer/footer";
@@ -7,12 +9,37 @@ import Header from "containers/header/header";
 import { Global } from "@emotion/core";
 import { globalStyles } from "styles";
 
+// if we're in dev mode
+if (process.env.NODE_ENV === "development") {
+  // Set up react-axe to catch accessibility issues during development
+  const axe = require("react-axe");
+  axe(React, ReactDOM, 1000);
+}
+
 const Main = styled.main`
   margin: 0 auto;
 `;
 
 const Layout = ({ children }) => {
   const [loading, setLoading] = useState(true);
+  let AOS;
+  useEffect(() => {
+    /**
+     * Server-side rendering does not provide the 'document' object
+     * therefore this import is required either in useEffect or componentDidMount as they
+     * are exclusively executed on a client
+     */
+    const AOS = require("aos");
+    AOS.init({
+      once: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (AOS) {
+      AOS.refresh();
+    }
+  });
 
   return (
     <React.Fragment>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { graphql } from "gatsby";
 import Layout from "components/layout";
 import SEO from "components/seo";
 import Hero from "containers/hero/hero";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import About from "containers/about/about";
 import Contact from "containers/contact/contact";
 import Skills from "components/skills/skills";
-
-// if we're in dev mode
-if (process.env.NODE_ENV === "development") {
-  // Set up react-axe to catch accessibility issues during development
-  const axe = require("react-axe");
-  axe(React, ReactDOM, 1000);
-}
-
-AOS.init({
-  once: true,
-});
 
 const IndexPage = () => {
   return (
